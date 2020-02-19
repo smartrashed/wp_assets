3b14a69327aa58ee7bc13ab2a50c19c0f783695c
Mohammad Mainuddin <smartrashed.rashed@gmail.com>
2020-02-19 12:42:23 +0600

    Items per page dropdown
    Items Per Page For List Table Hook

---
+++ Items_Per_Page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammadmainuddin
+ * Date: 2020-02-19
+ * Time: 12:38
+ *
+ * This solution for items per page in wp list table while using hook for custom post type
+ */
+
+
+add_action('restrict_manage_posts','gwf_items_per_page_filtering');
+function gwf_items_per_page_filtering($post_type){
+    if('gainformspost' !== $post_type){
+        return; //filter post
+    }
+    if(isset($_GET['pageVal'])){
+        $items_per_page = esc_sql($_GET['pageVal']);
+    }else{
+        $items_per_page = 5;
+    }
+    $paged = 1;
+    query_posts(array( 'showposts'=> $items_per_page,
+        'post_type' => 'gainformspost',
+        'posts_per_page' => $items_per_page,
+        'paged' => get_query_var('paged')
+    ));
+    ?>
+    <select class='page-select' name="pageVal">
+        <option value='5'><?php echo __('Select','gainforms'); ?></option>
+        <option value='5'><?php echo __('5 Items per page','gainforms'); ?></option>
+        <option value='10'><?php echo __('10 Items per page','gainforms'); ?></option>
+        <option value='15'><?php echo __('15 Items per page','gainforms'); ?></option>
+        <option value='25'><?php echo __('25 Items per page','gainforms'); ?></option>
+        <option value='50'><?php echo __('50 Items per page','gainforms'); ?></option>
+    </select>
+    <?php
+}
